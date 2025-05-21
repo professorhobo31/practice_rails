54d3b70144d2a186fbc7dfc795aaaa187fdb544b
Max <maxigiaco94@hotmail.com>
2025-05-21 14:53:16 -0300

    Comment out alerts

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,7 +1,7 @@
 class MainController < ApplicationController # we need to inherit from this class to get all the Rails functionality
   def index
-    flash.now[:notice] = "Ud. se ha logueado exitosamente"            # These flashes are inherited from the main controller. We pass a name thru a symbol
-    flash.now[:alert] = "El mail o la contraseña son incorrectos"     # They are meant to make our main landing page render these alrerts, but not so our about page
-    flash.now[:warning] = "Algo salió mal, recargue la página"
+    # flash.now[:notice] = "Ud. se ha logueado exitosamente"            # These flashes are inherited from the main controller. We pass a name thru a symbol
+    # flash.now[:alert] = "El mail o la contraseña son incorrectos"     # They are meant to make our main landing page render these alrerts, but not so our about page
+    # flash.now[:warning] = "Algo salió mal, recargue la página"
   end
 end
