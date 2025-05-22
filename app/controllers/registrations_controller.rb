8d026fd782c1dc7e63870b60121de775acc2ee1a
Max <maxigiaco94@hotmail.com>
2025-05-21 23:44:24 -0300

    Make forms register in our DB

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,20 @@
 class RegistrationsController < ApplicationController # we need to inherit from this class to get all the Rails functionality
   def new
-    @user = User.new    # We use an instance variable (with the @) so that this variable is available outside the scope of this method
+    @user = User.new    # We use an instance variable (with the @) so that this variable is available outside the scope of this method. It'll be usable in all views!
+  end
+
+  def create
+    @user = User.new(user_params)  # When we submit a form, all the data that we input will be stored in the params hash, but passing it here would be unsafe
+    if @user.save                  # This conditional checks wether the input data goes through and actually gets saved to our DB
+      redirect_to root_path, notice: "Cuenta creada"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 end
