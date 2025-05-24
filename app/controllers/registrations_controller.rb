17a1e11541883d6d26841af8d9050e53455869af
Max <maxigiaco94@hotmail.com>
2025-05-24 17:14:41 -0300

    Fix alerts not firing when form was incorrect
    As it turns out, in our registrations_controller in the create action
    on the else block we NEED to add "status: 400" (technically it should be
    any appropiate status number in the 400 range) on our render line.
    
    Otherwise our previous flash[:alert] line won't ever fire. Apparently it
    has something to do with how the HTTP requests interact. Was a bit hard
    to track down so it deserved it's own special commit

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -6,15 +6,16 @@ class RegistrationsController < ApplicationController # we need to inherit from 
   def create
     @user = User.new(user_params)  # When we submit a form, all the data that we input will be stored in the params hash, but passing it here would be unsafe
     if @user.save                  # This conditional checks wether the input data goes through and actually gets saved to our DB
-      redirect_to root_path, notice: "Cuenta creada"
+      redirect_to root_path, notice: "Su cuenta ha sido creada"
     else
-      render :new
+      flash[:alert] = "Algo salio mal. Intente nuevamente"
+      render :new, status: 400                  # Will go to app/views/registrations/new.html.erb and render the form again. IF WE DON'T ADD A STATUS, OUR ERROR FORMS WON'T SHOW UP
     end
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation)
-  end
+    params.require(:user).permit(:email, :password, :password_confirmation) # For security, this is a private method that we pass to the public create section. The require raises error if user
+  end                                                                       # is not found. The ":email, ..." parameters we pass are the only thing we'll allow our form to set in our DB
 end
