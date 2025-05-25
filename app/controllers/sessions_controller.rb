41a055b887b217f665f2a77bb7cd12de8a831360
Max <maxigiaco94@hotmail.com>
2025-05-25 20:58:11 -0300

    Add login page
    Added routes to login, edited sessions_controller to handle the new
    logic, checked it worked. Also added some basic bootstrap style to the
    logout button.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,18 @@
 class SessionsController < ApplicationController # we need to inherit from this class to get all the Rails functionality
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user.present? && user.authenticate(params[:password])  # This logic checks that the secure password matches our user before logging them in
+      session[:user_id] = user.id
+      redirect_to root_path, notice: "Bienvenido de vuelta"
+    else
+      flash[:alert] = "Email o contraseña inválidos"
+      render :new, status: 400
+    end
+  end
+
   def destroy
     session[:user_id] = nil
     redirect_to root_path, notice: "Su sesión ha sido cerrada"
