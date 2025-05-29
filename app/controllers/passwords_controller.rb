5b64d6867d0a8141c4e34f33bbc39cb2b1fab304
Max <maxigiaco94@hotmail.com>
2025-05-28 21:25:54 -0300

    Add password change form
    Add a form and functionality to have the user be able to edit their
    password after the initial creation of their user.

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -1,9 +1,20 @@
 class PasswordsController < ApplicationController
   before_action :require_user_logged_in   # we want only logged in users to have access to these actions. So we use a before_action and we go to application_controller to create the require_user_logged_in method, so that it is available in all controllers such as this one. Our new method should prevent execution of the bottom chunks if not a logged user
 
+  def update
+    if Current.user.update(password_params)
+      redirect_to root_path, notice: "Contraseña Actualizada"
+    else
+      render :edit
+    end
+  end
+
   def edit
   end
 
-  def update
+  private
+
+  def password_params
+    params.require(:user).permit(:password, :password_confirmation)
   end
 end
